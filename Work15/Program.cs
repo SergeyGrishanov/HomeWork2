b82dacd95bbd33cd9c4b7b0322a485aa53f5b556
Sergey <blackwhiterainbow@yandex.ru>
2023-01-18 21:32:37 +0300

    add нахождение субботы и воскресенья

---
+++ Work15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sub = 6;
+int sun = 7;
+
+if(num == sub || num == sun)
+{
+    Console.WriteLine($"Да");
+}
+else
+{
+    Console.WriteLine($"Нет");
+}
