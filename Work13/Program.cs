6e2db24237e42e258854f907b97058e5ce30695b
Sergey <blackwhiterainbow@yandex.ru>
2023-01-18 22:36:09 +0300

    add найдено решение.ю но выскакивает строка "Третье число: 0"

---
+++ Work13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Цифры считать справа налево).
+Console.Clear();
+
+Console.WriteLine("Введите любое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int result = number / 100;
+
+while (result > 10)
+{
+    result = result % 10;
+}
+Console.WriteLine($"Третья цифра: {result}");
+
+if (result == 0)
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
+
+
