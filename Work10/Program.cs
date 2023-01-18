0c5bdc0631ebc0913ccd688cd50c910dddd5714a
Sergey <blackwhiterainbow@yandex.ru>
2023-01-18 21:41:56 +0300

    add нахождение второй цифры в трехзначном числе

---
+++ Work10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a / 10;
+int result = b % 10;
+
+Console.WriteLine($"Трехзначное число {a} => Второе число {result}");
